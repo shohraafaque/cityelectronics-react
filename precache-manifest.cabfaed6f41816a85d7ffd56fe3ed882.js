21db27d22138a9967533814caea9566b39410e8a
Shohra Afaque <shohrazone@gmail.com>
2020-03-20 02:43:08 -0700

    Updates

---
+++ precache-manifest.cabfaed6f41816a85d7ffd56fe3ed882.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1931b1355aaf60a79af754d7edff411b",
+    "url": "/cityelectronics-react/index.html"
+  },
+  {
+    "revision": "1ffa6b6d240e329767af",
+    "url": "/cityelectronics-react/static/css/2.b706bf63.chunk.css"
+  },
+  {
+    "revision": "4830998878ac4ba0aa6a",
+    "url": "/cityelectronics-react/static/css/main.a1e3d7cc.chunk.css"
+  },
+  {
+    "revision": "1ffa6b6d240e329767af",
+    "url": "/cityelectronics-react/static/js/2.455d6670.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/cityelectronics-react/static/js/2.455d6670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4830998878ac4ba0aa6a",
+    "url": "/cityelectronics-react/static/js/main.3abb4901.chunk.js"
+  },
+  {
+    "revision": "d0fdc6de1295f89dea71",
+    "url": "/cityelectronics-react/static/js/runtime-main.d323c0fd.js"
+  },
+  {
+    "revision": "d4a8db3dbf45c5faad701ac5af1b4f0d",
+    "url": "/cityelectronics-react/static/media/Footer Adornment.d4a8db3d.svg"
+  },
+  {
+    "revision": "06412bd8ae8f9110994a56e33b4f8c85",
+    "url": "/cityelectronics-react/static/media/clock.06412bd8.svg"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/cityelectronics-react/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "83f976f576abb5aa563c02b971bd82cf",
+    "url": "/cityelectronics-react/static/media/guarantee.83f976f5.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/cityelectronics-react/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/cityelectronics-react/static/media/twitter.435fc4bc.svg"
+  }
+]);
